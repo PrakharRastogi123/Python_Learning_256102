b9fce81a6c8c2b96f50961d8b4794a205a79ae90
Prakhar Rastogi <prkharrastogi99.pr@gmail.com>
2021-04-26 02:07:08 +0530

    Code

---
+++ 256102_Daily_Practice/code5.py
@@ -0,0 +1,8 @@
+thousand = "(?:(M){0,3})?"
+hundred  = "(?:(D?(C){0,3})|(CM)|(CD))?"
+ten      = "(?:(L?(X){0,3})|(XC)|(XL))?"
+unit     = "(?:(V?(I){0,3})|(IX)|(IV))?"
+
+regex_pattern = r"^" + thousand + hundred + ten + unit + "$"
+
+import re
